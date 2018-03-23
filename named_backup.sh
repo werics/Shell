d5978b8a8ce21b856b3dcd44747cf33e4054aed8
werics <ericwu6060@gmail.com>
2018-03-23 11:39:21 +0800

    Backup for Bind

---
+++ named_backup.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+DATE=`date +%Y%m%d`
+DST_DIR='/data/backup/named'
+RSYNCDSECRET='/home/wuhesong/rsyncd.secret'
+
+
+cp -rfp /etc/named.conf $DST_DIR
+cp -rfp /var/named/*.db $DST_DIR
+cp -rfp /var/named/named.ca $DST_DIR
+cp -rfp /var/named/named_dump.db $DST_DIR
+cp -rfp /var/named/named.empty $DST_DIR
+cp -rfp /var/named/named.localhost $DST_DIR
+cp -rfp /var/named/named.loopback $DST_DIR
+
+tar zcvf /data/backup/$DATE.named.tar.gz $DST_DIR
+
+rsync -azuvP --progress /data/backup/*.tar.gz rsyncbak@172.25.15.24::dns --password-file=$RSYNCDSECRET
